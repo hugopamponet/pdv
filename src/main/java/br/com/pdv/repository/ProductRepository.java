776ce5d4d07f91592eae4233a1af2b80d2379133
hugopamponet <hugofarias9@gmail.com>
2025-09-26 12:06:37 -0300

    feat: creating the ProductRepository

---
+++ src/main/java/br/com/pdv/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package br.com.pdv.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<ProductRepository, String>{
+
+}
