258278804f0f7f3e3ecaa5eea868cb4a06dd175a
hugopamponet <hugofarias9@gmail.com>
2025-09-26 09:09:15 -0300

    feat: StrockModel creation

---
+++ src/main/java/br/com/pdv/model/StockModel.java
@@ -0,0 +1,81 @@
+package br.com.pdv.model;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+public class StockModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private String id;
+	
+	private Long amount;
+	private String purchasePrice;
+	private String salePrice;
+	private String unitOfMeasurement;
+	private String entryAndExit;
+	
+	public StockModel() {
+		
+	}
+
+	public StockModel(String id, Long amount, String purchasePrice, String salePrice, String unitOfMeasurement,
+			String entryAndExit) {
+		this.id = id;
+		this.amount = amount;
+		this.purchasePrice = purchasePrice;
+		this.salePrice = salePrice;
+		this.unitOfMeasurement = unitOfMeasurement;
+		this.entryAndExit = entryAndExit;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Long getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Long amount) {
+		this.amount = amount;
+	}
+
+	public String getPurchasePrice() {
+		return purchasePrice;
+	}
+
+	public void setPurchasePrice(String purchasePrice) {
+		this.purchasePrice = purchasePrice;
+	}
+
+	public String getSalePrice() {
+		return salePrice;
+	}
+
+	public void setSalePrice(String salePrice) {
+		this.salePrice = salePrice;
+	}
+
+	public String getUnitOfMeasurement() {
+		return unitOfMeasurement;
+	}
+
+	public void setUnitOfMeasurement(String unitOfMeasurement) {
+		this.unitOfMeasurement = unitOfMeasurement;
+	}
+
+	public String getEntryAndExit() {
+		return entryAndExit;
+	}
+
+	public void setEntryAndExit(String entryAndExit) {
+		this.entryAndExit = entryAndExit;
+	}
+}
