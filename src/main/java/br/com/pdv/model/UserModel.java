ce3784fb9d38cbadea5f08c87dddb2f495147ed4
hugopamponet <hugofarias9@gmail.com>
2025-09-26 08:43:55 -0300

    feat: UserModel creation

---
+++ src/main/java/br/com/pdv/model/UserModel.java
@@ -0,0 +1,81 @@
+package br.com.pdv.model;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+public class UserModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private String id;
+	
+	private String name;
+	private String email;
+	private String username;
+	private String password;
+	private String store;
+	
+	public UserModel() {
+		
+	}
+
+	public UserModel(String id, String name, String email, 
+			String username, String password, String store) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.username = username;
+		this.password = password;
+		this.store = store;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getStore() {
+		return store;
+	}
+
+	public void setStore(String store) {
+		this.store = store;
+	}
+}
