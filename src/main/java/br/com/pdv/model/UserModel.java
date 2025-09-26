73c5b1572263f0b82403f280c443df669f328a3e
hugopamponet <hugofarias9@gmail.com>
2025-09-26 09:23:16 -0300

    Entity configuration in the model package

--- src/main/java/br/com/pdv/model/UserModel.java
+++ src/main/java/br/com/pdv/model/UserModel.java
@@ -2,9 +2,11 @@ package br.com.pdv.model;
 
 import org.springframework.data.annotation.Id;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 
+@Entity
 public class UserModel {
 
 	@Id
